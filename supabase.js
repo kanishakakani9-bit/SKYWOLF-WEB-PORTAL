b1df84e7d630f37108d6c2d0a01ef101e580ef1d
kanishakakani9-bit <kanishakakani9@gmail.com>
2026-09-05 20:40:31 -0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+
+const SUPABASE_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
+
+
+const supabaseClient =
+    supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
